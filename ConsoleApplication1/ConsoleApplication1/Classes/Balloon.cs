ebc3b5f1f3e450066dfdb5d31984f4f97c586910
TaylorS <tSchumacher255@gmail.com>
2016-10-29 16:05:13 -0500

    Gabe: Homework due monday wtih Austin.
    Please override the toString method in your baloon class. Have the balloon get added to the list of balloons. Then print it out at the end. Also look up and implement tryParse

---
+++ ConsoleApplication1/ConsoleApplication1/Classes/Balloon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Classes
+{
+    public class Balloon
+    {
+        private string _color;
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        private string _size;
+
+        public string Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+
+        private int _psi;
+
+        public int Psi
+        {
+            get { return _psi; }
+            set { _psi = value; }
+        }
+
+        public Balloon(string color, string size, int psi) 
+        {
+            if(color == "yellow")
+                throw new ArgumentException("Yellow is fucking terrible!", "color");
+            Color = color;
+
+            if (size == "large" || size == "medium" || size == "small")
+                Size = size;
+            else
+                throw new ArgumentException("Not an option, try again", "size");
+            /*
+            if(size != "medium")
+                throw new ArgumentException("Not an option, try again", "size");
+            if(size != "small")
+                throw new ArgumentException("Not an option, try again", "size");
+             */
+            Size = size;
+
+            if (psi < 0)
+                throw new ArgumentException("A positive number is needed here", "psi");
+            Psi = psi;
+        }
+
+
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleApplication1.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputColor = "";
+            string inputSize = "";
+            int inputPsi = 0;
+            
+            while ( inputColor != "pop it")
+            {
+                try
+                {
+                    Console.WriteLine("Enter a color.");
+                    inputColor = Console.ReadLine();
+
+                    Console.WriteLine("Pick a size: Large, Medium, or Small?");
+                    inputSize = Console.ReadLine();
+
+                    Console.WriteLine("How much air does the balloon need? Enter a psi.");
+                    inputPsi = int.Parse(Console.ReadLine());
+
+                    Balloon inputBalloon = new Balloon(inputColor, inputSize, inputPsi);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);                    
+                }
+                
+            }
+        }
+    }
+}
